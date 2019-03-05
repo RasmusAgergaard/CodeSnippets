793310a3c39c83014c2bc2c6c0d017a9a855f1e1
Rasmus Agergaard <rasmusap@gmail.com>
2019-03-05 15:24:43 +0100

    Add files via upload

---
+++ LinqAndLambda.cs
@@ -0,0 +1,79 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LinqAndLambda
+{
+    public class Item
+    {
+        public int ItemId { get; set; }
+        public string ItemName { get; set; }
+        public int ItemNumber { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /********** Method 1 **********/
+
+            //Populate the list with items
+            var items1 = new List<Item>()
+            {
+                {new Item() {ItemId = 1, ItemName = "Ball", ItemNumber = 8845 }},
+                {new Item() {ItemId = 2, ItemName = "Flower", ItemNumber = 8537 }},
+                {new Item() {ItemId = 3, ItemName = "Rock", ItemNumber = 5421 }},
+                {new Item() {ItemId = 4, ItemName = "Cup", ItemNumber = 1278 }},
+                {new Item() {ItemId = 5, ItemName = "Pen", ItemNumber = 9965 }},
+            };
+
+            //Create Query
+            var itemQuery1 = from i in items1
+                              where i.ItemName.Contains("o")
+                              orderby i.ItemId
+                              select i;
+
+            //Run though query and output the content
+            Console.WriteLine("Query 1");
+            foreach (var item in itemQuery1)
+            {
+                Console.WriteLine(item.ItemName);
+            }
+
+            
+            /********** Method 2 **********/
+
+            //var itemQuery2 = items.Where(FilterItems).OrderBy(OrderItemsByName);
+
+
+            /********** Method 3 **********/
+
+            //Populate the list with items
+            var items3 = new List<Item>()
+            {
+                {new Item() {ItemId = 1, ItemName = "Ball", ItemNumber = 8845 }},
+                {new Item() {ItemId = 2, ItemName = "Flower", ItemNumber = 8537 }},
+                {new Item() {ItemId = 3, ItemName = "Rock", ItemNumber = 5421 }},
+                {new Item() {ItemId = 4, ItemName = "Cup", ItemNumber = 1278 }},
+                {new Item() {ItemId = 5, ItemName = "Pen", ItemNumber = 9965 }},
+            };
+
+            //Create Query
+            var itemQuery3 = items3.Where(i => i.ItemName.Contains("o"))
+                                     .OrderBy(i => i.ItemName);
+
+            //Run though query and output the content
+            Console.WriteLine("Query 3");
+            foreach (var item in itemQuery3)
+            {
+                Console.WriteLine(item.ItemName);
+            }
+
+            Console.ReadLine();
+        }
+
+        //Used for method 2
+        private bool FilterItems(Item i) => i.ItemName.Contains("o");
+        private string OrderItemsByName(Item i) => i.ItemName;
+    }
+}
