1fb3b7485729db5767f2ac3fe44db9c310e4245c
Rasmus Agergaard <rasmusap@gmail.com>
2019-03-06 14:00:41 +0100

    Update to LinqAndLambda.cs

--- LinqAndLambda.cs
+++ LinqAndLambda.cs
@@ -40,10 +40,28 @@ namespace LinqAndLambda
                 Console.WriteLine(item.ItemName);
             }
 
-            
+
             /********** Method 2 **********/
 
-            //var itemQuery2 = items.Where(FilterItems).OrderBy(OrderItemsByName);
+            //Populate the list with items
+            var items2 = new List<Item>()
+            {
+                {new Item() {ItemId = 1, ItemName = "Ball", ItemNumber = 8845 }},
+                {new Item() {ItemId = 2, ItemName = "Flower", ItemNumber = 8537 }},
+                {new Item() {ItemId = 3, ItemName = "Rock", ItemNumber = 5421 }},
+                {new Item() {ItemId = 4, ItemName = "Cup", ItemNumber = 1278 }},
+                {new Item() {ItemId = 5, ItemName = "Pen", ItemNumber = 9965 }},
+            };
+
+            //Create Query
+            var itemQuery2 = items2.Where(FilterItems).OrderBy(OrderItemsByName);
+
+            //Run though query and output the content
+            Console.WriteLine("Query 2");
+            foreach (var item in itemQuery2)
+            {
+                Console.WriteLine(item.ItemName);
+            }
 
 
             /********** Method 3 **********/
@@ -73,7 +91,7 @@ namespace LinqAndLambda
         }
 
         //Used for method 2
-        private bool FilterItems(Item i) => i.ItemName.Contains("o");
-        private string OrderItemsByName(Item i) => i.ItemName;
+        private static bool FilterItems(Item i) => i.ItemName.Contains("o");
+        private static string OrderItemsByName(Item i) => i.ItemName;
     }
 }
