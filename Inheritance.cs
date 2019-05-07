4229c719c27c77150a6c19fab9baebfe66042162
Rasmus Agergaard <rasmusap@gmail.com>
2019-05-07 08:00:57 +0200

    Inheritance.cs updated
    Added abstract classes and methods

--- Inheritance.cs
+++ Inheritance.cs
@@ -1,73 +1,83 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace Inheritance
 {
-    public enum playerType { Warrior, Monk, Archer, Thief };
-    public enum enemyType { Giant, Wolf, Undead, Spider}
+    public enum creatureTypes { Warrior, Monk, Archer, Thief, Giant, Wolf, Undead, Spider };
 
-    public class Creature
+    public abstract class Creature
     {
         public int Health { get; set; }
+        public int Damage { get; set; }
+        public creatureTypes Type { get; set; }
+
+        abstract public void TakeDamage(int damage);
+        abstract public void Attack(Creature target);
     }
 
     public class Player : Creature
     {
-        public Player(int health, int mana, playerType type)
+        public Player(int health, int mana, int damage, creatureTypes type)
         {
             Health = health;
             Mana = mana;
+            Damage = damage;
             Type = type;
         }
 
         public int Mana { get; set; }
-        public playerType Type { get; set; }
+
+        public override void TakeDamage(int damage)
+        {
+            Health -= damage;
+            Console.WriteLine($"{Type} has {Health} health left\n");
+        }
+
+        public override void Attack(Creature target)
+        {
+            Console.WriteLine($"{Type} attacks {target.Type}");
+            target.TakeDamage(Damage);
+        }
     }
 
     public class Enemy : Creature
     {
-        public Enemy(int health, enemyType type)
+        public Enemy(int health, int damage, creatureTypes type)
         {
             Health = health;
+            Damage = damage;
             Type = type;
         }
 
-        public enemyType Type { get; set; }
+        public override void TakeDamage(int damage)
+        {
+            Health -= damage;
+            Console.WriteLine($"{Type} has {Health} health left\n");
+        }
+
+        public override void Attack(Creature target)
+        {
+            Console.WriteLine($"{Type} attacks {target.Type}");
+            target.TakeDamage(Damage);
+        }
     }
 
     class Program
     {
         static void Main(string[] args)
         {
-            var players = new List<Player>();
-            players.AddRange(new List<Player>
-            {
-                new Player(health: 80, mana: 50, type: playerType.Monk),
-                new Player(health: 150, mana: 0, type: playerType.Warrior),
-            });
+            var player1 = new Player(health: 80, mana: 50, damage: 10, type: creatureTypes.Monk);
+            var player2 = new Player(health: 120, mana: 0, damage: 25, type: creatureTypes.Warrior);
+            var enemy1 = new Enemy(health: 120, damage: 25, type: creatureTypes.Giant);
+            var enemy2 = new Enemy(health: 120, damage: 25, type: creatureTypes.Undead);
+            var enemy3 = new Enemy(health: 120, damage: 25, type: creatureTypes.Spider);
 
-            var enemies = new List<Enemy>();
-            enemies.AddRange(new List<Enemy>
-            {
-                new Enemy(health: 250, type: enemyType.Giant),
-                new Enemy(health: 80, type: enemyType.Undead),
-                new Enemy(health: 30, type: enemyType.Spider)
-            });
+            //Pretended game logic
+            player1.Attack(enemy1);
+            player2.Attack(enemy1);
 
-            Console.WriteLine("Players:");
-
-            foreach (var player in players)
-            {
-                Console.WriteLine($"Type: {player.Type}, Health: {player.Health}, Mana: {player.Mana}");
-            }
-
-            Console.WriteLine("\nEnemies:");
-
-            foreach (var enemy in enemies)
-            {
-                Console.WriteLine($"Type: {enemy.Type}, Health: {enemy.Health}");
-            }
+            enemy1.Attack(player2);
+            enemy2.Attack(player2);
+            enemy3.Attack(player2);
         }
     }
-
 }
