6b060eb6e5c28800db58a6017c8d9e4e99c590b0
Rasmus Agergaard <rasmusap@gmail.com>
2019-05-14 16:43:04 +0200

    DisposeAndFinalize.cs added

---
+++ DisposeAndFinalize.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace DisposeAndFinalize
+{
+    public class MyClass : IDisposable
+    {
+        //Constructor
+        public MyClass()
+        {
+            //Initialization
+        }
+
+        //Finalize (aka Destructor) 
+        ~MyClass()
+        {
+            Dispose();
+        }
+
+        public void Dispose()
+        {
+            //write code to release unmanaged resources
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
