076202c94a2c3ba345f824344617ec1c9a10f648
Rasmus Agergaard <rasmusap@gmail.com>
2019-05-20 07:53:56 +0200

    Partial.cs added

---
+++ Partial.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Partial
+{
+    //Partial class definition
+    public partial class Coords
+    {
+        private int x;
+        private int y;
+
+        public Coords(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    //Another partial class definition
+    public partial class Coords
+    {
+        public void SwitchCoords()
+        {
+            var temp = x;
+            x = y;
+            y = temp;
+        }
+    }
+
+    //Another partial class definition
+    public partial class Coords
+    {
+        public void PrintCoords()
+        {
+            Console.WriteLine($"Coords: {x}:{y}");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var testCoord = new Coords(240, 852);
+            testCoord.PrintCoords();
+            testCoord.SwitchCoords();
+            testCoord.PrintCoords();
+        }
+    }
+}
