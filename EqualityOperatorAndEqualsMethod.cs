a6e275023ad8b599ffcc1b232e998ecc2a8b7693
Rasmus Agergaard <rasmusap@gmail.com>
2019-05-21 11:48:29 +0200

    EqualityOperatorAndEqualsMethod.cs added

---
+++ EqualityOperatorAndEqualsMethod.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace EqualityOperatorAndEqualsMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name1 = "Bill";
+            string name2 = name1;
+
+            Console.WriteLine(name1 == name2);                          //Output: True
+            Console.WriteLine(name1.Equals(name2));                     //Output: True
+
+            object name11 = "Bill";
+            char[] values = new char[] { 'B', 'i', 'l', 'l' };
+            object name22 = new string(values);
+
+            Console.WriteLine(name11 == name22);                          //Output: False
+            Console.WriteLine(name11.Equals(name22));                     //Output: True
+        }
+    }
+}
