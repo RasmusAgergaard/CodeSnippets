126f0a403e019a1fe689f33adce2b097b9d46b78
Rasmus Agergaard <rasmusap@gmail.com>
2019-06-03 08:43:53 +0200

    Delegates4.cs added

---
+++ Delegates4.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace Delegates4
+{
+    public enum operations { Add, Sub, Mul, Div}
+
+    public class MathClass
+    {
+        public delegate int PointerMaths(int a, int b);
+
+        public PointerMaths GetPointer(operations operation)
+        {
+            PointerMaths pointerMaths = null;
+
+            switch (operation)
+            {
+                case operations.Add:
+                    pointerMaths = Add;
+                    break;
+                case operations.Sub:
+                    pointerMaths = Sub;
+                    break;
+                case operations.Mul:
+                    pointerMaths = Mul;
+                    break;
+                case operations.Div:
+                    pointerMaths = Div;
+                    break;
+                default:
+                    break;
+            }
+
+            return pointerMaths;
+        }
+
+        private static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        private static int Sub(int a, int b)
+        {
+            return a - b;
+        }
+
+        private static int Mul(int a, int b)
+        {
+            return a * b;
+        }
+
+        private static int Div(int a, int b)
+        {
+            return a / b;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var mathClass = new MathClass();
+
+            Console.WriteLine(mathClass.GetPointer(operations.Add).Invoke(5, 5));       //Output: 10
+            Console.WriteLine(mathClass.GetPointer(operations.Sub).Invoke(5, 5));       //Output: 0
+            Console.WriteLine(mathClass.GetPointer(operations.Mul).Invoke(5, 5));       //Output: 25
+            Console.WriteLine(mathClass.GetPointer(operations.Div).Invoke(5, 5));       //Output: 1
+
+        }
+    }
+}
