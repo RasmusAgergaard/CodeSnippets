b79624d0cf49e70213fdc1fa91401fb0deea3f53
Rasmus Agergaard <rasmusap@gmail.com>
2019-06-03 09:05:08 +0200

    Delegates4.cs updated

--- Delegates4.cs
+++ Delegates4.cs
@@ -2,7 +2,7 @@
 
 namespace Delegates4
 {
-    public enum operations { Add, Sub, Mul, Div}
+    public enum operations { Add, Sub, Mul, Div, All}
 
     public class MathClass
     {
@@ -26,6 +26,12 @@ namespace Delegates4
                 case operations.Div:
                     pointerMaths = Div;
                     break;
+                case operations.All:
+                    pointerMaths += Add;
+                    pointerMaths += Sub;
+                    pointerMaths += Mul;
+                    pointerMaths += Div;
+                    break;
                 default:
                     break;
             }
@@ -35,21 +41,25 @@ namespace Delegates4
 
         private static int Add(int a, int b)
         {
+            Console.Write("Add called ");
             return a + b;
         }
 
         private static int Sub(int a, int b)
         {
+            Console.Write("Sub called ");
             return a - b;
         }
 
         private static int Mul(int a, int b)
         {
+            Console.Write("Mul called ");
             return a * b;
         }
 
         private static int Div(int a, int b)
         {
+            Console.Write("Div called ");
             return a / b;
         }
     }
@@ -60,11 +70,12 @@ namespace Delegates4
         {
             var mathClass = new MathClass();
 
-            Console.WriteLine(mathClass.GetPointer(operations.Add).Invoke(5, 5));       //Output: 10
-            Console.WriteLine(mathClass.GetPointer(operations.Sub).Invoke(5, 5));       //Output: 0
-            Console.WriteLine(mathClass.GetPointer(operations.Mul).Invoke(5, 5));       //Output: 25
-            Console.WriteLine(mathClass.GetPointer(operations.Div).Invoke(5, 5));       //Output: 1
+            Console.WriteLine(mathClass.GetPointer(operations.Add).Invoke(5, 5));       //Output: Add called 10
+            Console.WriteLine(mathClass.GetPointer(operations.Sub).Invoke(5, 5));       //Output: Sub called 0
+            Console.WriteLine(mathClass.GetPointer(operations.Mul).Invoke(5, 5));       //Output: Mul called 25
+            Console.WriteLine(mathClass.GetPointer(operations.Div).Invoke(5, 5));       //Output: Div called 1
 
+            mathClass.GetPointer(operations.All).Invoke(5, 5);                          //Output: Add called Sub called Mul called Div called
         }
     }
 }
