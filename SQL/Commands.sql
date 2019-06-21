ab91f25f962a6bb643892f3a15746ed3356eb2e8
Rasmus Agergaard <rasmusap@gmail.com>
2019-06-21 14:19:46 +0200

    Formatting...

--- SQL/Commands.sql
+++ SQL/Commands.sql
@@ -12,7 +12,7 @@ CREATE TABLE Customer
 INSERT INTO dbo.Customer (FirstName, LastName, Age) values ('Joey', 'Blue', 40);
 
 /* Select */
-SELECT *												-- Returns all columns
+SELECT *									-- Returns all columns
 FROM Customer;
 
 SELECT FirstName, LastName, Age							-- Returns the 3 selected columns
@@ -25,7 +25,7 @@ AND LastName='Blue';
 
 SELECT FirstName, LastName, Age							-- Returns all where FirstName starts with 'J'
 FROM Customer
-WHERE FirstName like 'J%';								-- "%" returns everything, "_" returns at least one character
+WHERE FirstName like 'J%';							-- "%" returns everything, "_" returns at least one character
 
 /* Update */
 UPDATE Customer 
@@ -34,30 +34,30 @@ WHERE FirstName = 'Small'
 AND LastName = 'Blue';
 
 /* Delete */
-DELETE Customer;										-- Deletes all data in the table, but not the table itself
+DELETE Customer;								-- Deletes all data in the table, but not the table itself
 
 DELETE Customer
 WHERE FirstName = 'Paul'
-AND LastName LIKE 'H%';									-- Deletes all contacts the fits the WHERE
+AND LastName LIKE 'H%';								-- Deletes all contacts the fits the WHERE
 
 /* Drop */
-DROP TABLE Customer;									-- Deletes the table and all data in it
+DROP TABLE Customer;								-- Deletes the table and all data in it
 
 /* Alter */
 ALTER TABLE dbo.Customer
-ADD City VARCHAR(50);									-- Adds a City column to the table
+ADD City VARCHAR(50);								-- Adds a City column to the table
 
 /* Add Foreign Key */
 ALTER TABLE Orders
-ADD FOREIGN KEY (CustomerId) REFERENCES Customer(Id);	-- Adds a reference from CustomerId to the Id column in the Customer table
+ADD FOREIGN KEY (CustomerId) REFERENCES Customer(Id);				-- Adds a reference from CustomerId to the Id column in the Customer table
 
 /* Join */
 SELECT * FROM Orders
-INNER JOIN Product ON Orders.ProduceId = Product.Id		-- Shows the columns from the Product table after the order columns
+INNER JOIN Product ON Orders.ProduceId = Product.Id				-- Shows the columns from the Product table after the order columns
 
 /* Group By */
 SELECT Customer.LastName, SUM(Product.Price) AS Total
 FROM Orders
 INNER JOIN Product ON Orders.ProduceId = Product.Id
 INNER JOIN Customer ON Orders.CustomerId = Customer.Id
-GROUP BY Customer.LastName;								-- Shows the sum of money spend, grouped by customer lastname
+GROUP BY Customer.LastName;							-- Shows the sum of money spend, grouped by customer lastname
