6cd32be09a877c8108c9f6c72c66940959feb369
RasmusAgergaard <rasmusap@gmail.com>
2019-07-05 14:13:22 +0200

    SQL Server db added

--- ASP.NET Core/OdeToFood/OdeToFood/Pages/Restaurants/Edit.cshtml.cs
+++ ASP.NET Core/OdeToFood/OdeToFood/Pages/Restaurants/Edit.cshtml.cs
@@ -25,11 +25,20 @@ namespace OdeToFood.Pages.Restaurants
             this.htmlHelper = htmlHelper;
         }
 
-        public IActionResult OnGet(int restaurantId)
+        public IActionResult OnGet(int? restaurantId)
         {
             Cuisines = htmlHelper.GetEnumSelectList<CuisineType>();
-            Restaurant = restaurantData.GetById(restaurantId);
 
+            if (restaurantId.HasValue)
+            {
+                Restaurant = restaurantData.GetById(restaurantId.Value);
+            }
+            else
+            {
+                Restaurant = new Restaurant();
+            }
+
+            
             if (Restaurant == null)
             {
                 return RedirectToPage("./NotFound");
@@ -40,9 +49,24 @@ namespace OdeToFood.Pages.Restaurants
 
         public IActionResult OnPost()
         {
-            Restaurant = restaurantData.Update(Restaurant);
+            if (!ModelState.IsValid)
+            {
+                Cuisines = htmlHelper.GetEnumSelectList<CuisineType>();
+                return Page();
+            }
+
+            if (Restaurant.Id > 0)
+            {
+                restaurantData.Update(Restaurant);
+            }
+            else
+            {
+                restaurantData.Add(Restaurant);
+            }
+            
             restaurantData.Commit();
-            return Page();
+            TempData["Message"] = "Restaurant saved!";
+            return RedirectToPage("./Detail", new { restaurantId = Restaurant.Id });
         }
     }
 }
