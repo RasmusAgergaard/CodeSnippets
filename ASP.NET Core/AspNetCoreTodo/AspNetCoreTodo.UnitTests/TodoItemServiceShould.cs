1dbb9769e623f021fb6b811219fff1ccc74744eb
RasmusAgergaard <rasmusap@gmail.com>
2019-08-08 14:47:59 +0200

    Updated

---
+++ ASP.NET Core/AspNetCoreTodo/AspNetCoreTodo.UnitTests/TodoItemServiceShould.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading.Tasks;
+using AspNetCoreTodo.Data;
+using AspNetCoreTodo.Models;
+using AspNetCoreTodo.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+
+namespace AspNetCoreTodo.UnitTests
+{
+    public class TodoItemServiceShould
+    {
+        [Fact]
+        public async Task AddNewItemAsIncompleteWithDueDate()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>().UseInMemoryDatabase(databaseName: "Test_AddNewItem").Options;
+
+            using (var context = new ApplicationDbContext(options))
+            {
+                var service = new TodoItemService(context);
+
+                var fakeUser = new IdentityUser
+                {
+                    Id = "fake-000",
+                    UserName = "fake@example.com"
+                };
+
+                await service.AddItemAsync(new TodoItem
+                {
+                    Title = "Testing"
+                }, fakeUser);
+            };
+        }
+    }
+}
