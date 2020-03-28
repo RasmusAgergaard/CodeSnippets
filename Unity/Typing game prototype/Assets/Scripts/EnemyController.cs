c7b4b94114c92cf8f1a6c49b21fa6a56fb27c423
Rasmus Agergaard <rasmusap@gmail.com>
2020-03-28 14:49:19 +0100

    Typing game prototype added

---
+++ Unity/Typing game prototype/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class EnemyController : MonoBehaviour
+{
+    public float speed = 1.0f;
+    private Transform _playerTransform;
+    private List<string> _words;
+    public string word;
+    private TextMesh _text;
+
+    public EnemyController()
+    {
+        _words = new List<string>()
+        {
+            "Word",
+            "Dead",
+            "Cow",
+            "Ping",
+            "Flower",
+            "Car",
+            "Bob"
+        };
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _playerTransform = GameObject.FindWithTag("Player").GetComponent<Transform>();
+
+        word = _words[Random.Range(0, _words.Count)];
+
+        _text = GetComponentInChildren<TextMesh>();
+        _text.text = word;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveTowardsPlayer();
+    }
+
+    private void MoveTowardsPlayer()
+    {
+        if (Vector3.Distance(transform.position, _playerTransform.position) > 0.001f)
+        {
+            float step = speed * Time.deltaTime; // calculate distance to move
+            var targetFixed = new Vector3(_playerTransform.position.x, transform.position.y, _playerTransform.position.z); //Only move on the x and z axis
+            transform.position = Vector3.MoveTowards(transform.position, targetFixed, step);
+        }
+    }
+}
---
+++ Unity/Typing game prototype/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    public string letters;
+    public Text text;
+
+    private List<GameObject> _enemies;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        letters = "";
+        text = GameObject.Find("UiText").GetComponent<Text>();
+        FindEnemies();
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        LookForKeyboardInputs();
+
+        LookForEnemyMatch();
+
+        UpdateUi();
+    }
+
+    private void LookForEnemyMatch()
+    {
+        if (Input.anyKeyDown)
+        {
+            FindEnemies();
+
+            foreach (var enemy in _enemies)
+            {
+                //TODO: Fix this mess
+                //var enemyText = enemy.word
+            }
+
+            
+        }
+    }
+
+    private void UpdateUi()
+    {
+        text.text = letters;
+    }
+
+    private void LookForKeyboardInputs()
+    {
+        if (Input.anyKeyDown)
+        {
+            foreach (KeyCode kcode in Enum.GetValues(typeof(KeyCode)))
+            {
+                if (Input.GetKey(kcode))
+                {
+                    if (kcode.ToString() == "Backspace")
+                    {
+                        letters = "";
+                    }
+                    else
+                    {
+                        letters += kcode;
+                    }
+                }
+            }
+        }
+    }
+
+    private void FindEnemies()
+    {
+        var enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        _enemies = enemies.ToList();
+    }
+}
